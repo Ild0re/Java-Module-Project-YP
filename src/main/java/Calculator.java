2646753cc64a211fd1508dcb840d1c860bb2be29
Ildar <nuruda@yandex.ru>
2023-12-28 18:45:20 +0300

    Corrected some logic

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -13,22 +13,38 @@ public class Calculator {
     }
 
     void toSumItems() {
-        Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.println("Введите название товара:");
-            String item = scanner.next();
-            System.out.println("Введите стоимость товара:");
-            double itemCost = scanner.nextDouble();
-            Product newProduct = new Product(item, itemCost);
-            System.out.println("Товар успешно добавлен.");
-            sum += newProduct.cost;
-            items.add(newProduct);
-            System.out.println("Хотите ввести еще товар?");
-            String answer = scanner.next();
-            if (answer.equalsIgnoreCase("Завершить")) {
-                scanner.close();
-                break;
+            Scanner scanner = new Scanner(System.in);
+            String item = toDefineName(scanner);
+            double itemCost = toDefineCost(scanner);
+            if (itemCost < 0) {
+                System.out.println("Введите положительное число!");
+            } else {
+                Product newProduct = new Product(item, itemCost);
+                System.out.println("Товар успешно добавлен.");
+                sum += newProduct.cost;
+                items.add(newProduct);
+                System.out.println("Хотите ввести еще товар? Для выхода введите слово Завершить");
+                String answer = scanner.next();
+                if (answer.equalsIgnoreCase("Завершить")) {
+                    scanner.close();
+                    break;
+                }
             }
         }
+    }
+
+    String toDefineName(Scanner scanner) {
+        System.out.println("Введите название товара:");
+        return scanner.nextLine();
+    }
+
+    Double toDefineCost(Scanner scanner) {
+        System.out.println("Введите стоимость товара:");
+        while (!scanner.hasNextDouble()) {
+            System.out.println("Введите число!");
+            scanner.nextLine();
+        }
+        return scanner.nextDouble();
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,26 +5,25 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Привет! На сколько человек разделить счёт?");
         while (true) {
-            String blank = scanner.next();
-            try {
-                int guests = Integer.parseInt(blank);
-                if (guests < 1) {
-                    System.out.println("Введите, пожалуйста, корректное количество гостей");
-                } else if (guests == 1) {
-                    System.out.println("Для разделения счёта необходимо ввести более одного гостя");
-                } else {
-                    Calculator calculator = new Calculator(guests);
-                    calculator.toSumItems();
-                    Formatter formatter = new Formatter();
-                    double result = formatter.toRound(calculator.sum / guests);
-                    System.out.println("Добавленные товары: ");
-                    Formatter.printStrings(calculator.items);
-                    System.out.println("\nСумма к оплате: " + result + " " +
-                            formatter.toFormatRubles(result));
-                    break;
-                }
-            } catch (NumberFormatException ex) {
+            while (!scanner.hasNextInt()) {
                 System.out.println("Пожалуйста, введите число!");
+                scanner.nextLine();
+            }
+            int guests = scanner.nextInt();
+            if (guests < 1) {
+                System.out.println("Введите, пожалуйста, корректное количество гостей");
+            } else if (guests == 1) {
+                System.out.println("Для разделения счёта необходимо ввести более одного гостя");
+            } else {
+                Calculator calculator = new Calculator(guests);
+                calculator.toSumItems();
+                Formatter formatter = new Formatter();
+                double result = formatter.toRound(calculator.sum / guests);
+                System.out.println("Добавленные товары: ");
+                Formatter.printStrings(calculator.items);
+                System.out.println("\nСумма к оплате: " + result + " " +
+                        formatter.toFormatRubles(result));
+                break;
             }
         }
         scanner.close();
